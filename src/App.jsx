a80be919a2a4cb5d00f867d42a876b484061d919
Tina Flower <tmflower@gmail.com>
2023-03-28 10:33:20 -1000

    add google authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route, NavLink, useNavigate } from 'react-router-dom';
+import { auth } from '../firebase-config';
+import { signOut } from "firebase/auth";
 import './App.css'
+import { Home } from './Home.jsx';
+import { Login } from './Login';
+import { Post } from './Post';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isAuth, setIsAuth] = useState(false);
+
+	async function logout() {
+		await signOut(auth);
+		localStorage.clear();
+		setIsAuth(false);
+		window.location.pathname = "/login";
+	}
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+			<nav>
+				<NavLink to="/"> Home </NavLink>
+				{isAuth ? <NavLink to="/post"> Post </NavLink> : null}
+				{!isAuth ? <NavLink to="/login"> Login </NavLink> : <button id="logout-btn" onClick={logout}><NavLink>Logout</NavLink></button>}
+			</nav>
+			<Routes>
+				<Route path="/" element={<Home/>}></Route>
+				<Route path="/login" element={<Login setIsAuth={setIsAuth}/>}></Route>
+				<Route path="/post" element={<Post/>}></Route>
+			</Routes>
+		</BrowserRouter>
   )
 }
 
-export default App
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,22 @@
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+export function Login({ setIsAuth }) {
+
+	const navigate = useNavigate();
+
+	async function loginWithGoogle() {
+		await signInWithPopup(auth, provider);
+		localStorage.setItem("isAuth", true);
+		setIsAuth(true);
+		navigate('/');
+	}
+
+	return (
+		<div className="login">
+			<h1>Sign in with Google</h1>
+			<button className="login-with-google-btn" onClick={loginWithGoogle}>Sign in with Google</button>
+		</div>
+	)
+}
