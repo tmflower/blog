aa238a1f2cf2d43d8255ce2aaa49a60ffd345255
Tina Flower <tmflower@gmail.com>
2023-04-12 08:07:14 -1000

    remove comments

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAF16GP0GKGK4pb3trpmxQ65Ai1k5IxR8M",
   authDomain: "blog-746d1.firebaseapp.com",
